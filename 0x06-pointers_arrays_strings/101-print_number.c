a30eba3ce1d547b3a6e78fd86b9f8654884fd31b
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-03-02 14:06:33 -0800

    a function that prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,20 +9,15 @@
 
 void print_number(int n)
 {
-	int divisor = 1;
-
 	if (n < 0)
 	{
 	_putchar('-');
 	n *= -1;
 	}
-
-	while (n / divisor >= 10)
-	divisor *= 10;
-
-	while (divisor != 0)
+	
+	if (n / 10)
 	{
-		_putchar(n / divisor % 10 + '0');
-		divisor /= 10;
+		print_number(n / 10);
 	}
+	_putchar(n % 10 + '0');
 }
