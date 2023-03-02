bd91f29c888398cd0af75503059543d98d493b12
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-03-02 14:21:46 -0800

    a function that prints an integer refactored

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
-* print_number - Prints an integer using only _putchar
-* @n: The integer to be printed
-*
-* Return: void
-*/
+ * print_number - Prints an integer using only _putchar
+ * @n: The integer to be printed
+ *
+ * Return: void
+ */
+
 
 void print_number(int n)
 {
-	unsigned int num = n;
-
 	if (n < 0)
 	{
 	_putchar('-');
-	num = -num;
+	n *= -1;
 	}
 
-	if ((num / 10) > 10)
+	if (n / 10)
 	{
-		print_number(num / 10);
+		print_number(n / 10);
 	}
-	_putchar((num % 10) + '0');
+	_putchar(n % 10 + '0');
 }
