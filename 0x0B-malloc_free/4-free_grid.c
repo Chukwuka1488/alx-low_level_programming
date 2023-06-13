483c4bd80a3391e29d2b15459728b304f3f5aab4
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-06-13 21:31:44 +0200

    Automatic and dynamic allocation, malloc and free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+/*
+ * File: 4-free_grid.c
+ * Author: Chukwuka Akibor
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid previously created by alloc_grid
+ * @grid: pointer to grid to free
+ * @height: height of the grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	/* Free each row of the grid */
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	/* Free the grid itself */
+	free(grid);
+}
