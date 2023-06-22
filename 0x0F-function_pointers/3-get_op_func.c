c960315f70acd9c62ca17789ac29e8822bf23fd1
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-06-22 16:57:57 +0200

    Write a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+#include "3-op_functions.c"
+
+/**
+ * get_op_func - Selects the correct function to perform
+ *               the operation asked by the user
+ * @s: operator passed as argument to the program
+ * 
+ * Return: pointer to the function that corresponds to the operator
+ *         given as a parameter, or NULL if no match found
+ */
+int (*get_op_func(char *s))(int, int) {
+    // Define an array of op_t structs
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i = 0;
+
+    // Find the operator in the ops array
+    while (ops[i].op != NULL && *ops[i].op != *s)
+        i++;
+
+    // Return the matching function
+    return (ops[i].f);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include "function_pointers.h"
+
+/**
+ * main - performs simple operations.
+ * @argc: number of arguments passed.
+ * @argv: array of pointers to the arguments.
+ *
+ * Return: 0 on success, exit with status 98, 99, or 100 otherwise.
+ */
+int main(int argc, char *argv[]) {
+    // Variables to hold numbers
+    int a, b;
+    // Function pointer to hold the operation function
+    int (*operation)(int, int);
+
+    // Check if the number of arguments is correct
+    if (argc != 4) {
+        printf("Error\n");
+        exit(98);
+    }
+
+    // Convert arguments to int
+    a = atoi(argv[1]);
+    b = atoi(argv[3]);
+
+    // Get the operation function
+    operation = get_op_func(argv[2]);
+
+    // If the operation is invalid or if the operator string contains more than one char
+    if (operation == NULL || (argv[2][1] != '\0')) {
+        printf("Error\n");
+        exit(99);
+    }
+
+    // If trying to divide by zero
+    if ((operation == op_div || operation == op_mod) && b == 0) {
+        printf("Error\n");
+        exit(100);
+    }
+
+    // Perform the operation and print the result
+    printf("%d\n", operation(a, b));
+
+    return (0);
+}
