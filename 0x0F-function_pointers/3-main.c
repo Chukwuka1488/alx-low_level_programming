f217b70b6992b8b0b49d0d0eb5e648695a28ad39
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-06-22 08:39:17 -0700

    Write a program that performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,46 +3,53 @@
 #include "3-calc.h"
 
 /**
- * main - performs simple operations.
- * @argc: number of arguments passed.
- * @argv: array of pointers to the arguments.
- *
- * Return: 0 on success, exit with status 98, 99, or 100 otherwise.
- */
-int main(int argc, char *argv[]) {
-    /* Variables to hold numbers */
-    int a, b;
-    /* Function pointer to hold the operation function */
-    int (*operation)(int, int);
+  * main - performs simple operations.
+  * @argc: number of arguments passed.
+  * @argv: array of pointers to the arguments.
+  *
+  * Return: 0 on success, exit with status 98, 99, or 100 otherwise.
+  */
 
-    /* Check if the number of arguments is correct */
-    if (argc != 4) {
-        printf("Error\n");
-        exit(98);
-    }
+int main(int argc, char *argv[])
+{
+	/* Variables to hold numbers */
+	int a, b;
+	/* Function pointer to hold the operation function */
+	int (*operation)(int, int);
 
-    /* Convert arguments to int */
-    a = atoi(argv[1]);
-    b = atoi(argv[3]);
+	/* Check if the number of arguments is correct */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    /* Get the operation function */
-    operation = get_op_func(argv[2]);
+	/* Convert arguments to int */
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-    /* If the operation is invalid or if the operator 
-       string contains more than one char */
-    if (operation == NULL || (argv[2][1] != '\0')) {
-        printf("Error\n");
-        exit(99);
-    }
+	/* Get the operation function */
+	operation = get_op_func(argv[2]);
 
-    /* If trying to divide by zero */
-    if ((operation == op_div || operation == op_mod) && b == 0) {
-        printf("Error\n");
-        exit(100);
-    }
+	/**
+	  * If the operation is invalid or if the operator
+	  * string contains more than one char
+	 */
+	if (operation == NULL || (argv[2][1] != '\0'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    /* Perform the operation and print the result */
-    printf("%d\n", operation(a, b));
+	/* If trying to divide by zero */
+	if ((operation == op_div || operation == op_mod) && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    return (0);
+	/* Perform the operation and print the result */
+	printf("%d\n", operation(a, b));
+
+	return (0);
 }
