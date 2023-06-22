7dc20fb99d452cab209a8615c60cfd8aae65580a
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-06-22 10:20:57 -0700

    Write a program that performs simple operations.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,26 +5,28 @@
  * get_op_func - Selects the correct function to perform
  *               the operation asked by the user
  * @s: operator passed as argument to the program
- * 
+ *
  * Return: pointer to the function that corresponds to the operator
  *         given as a parameter, or NULL if no match found
  */
-int (*get_op_func(char *s))(int, int) {
-    /* Define an array of op_t structs */
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+int (*get_op_func(char *s))(int, int)
+{
+	/* Define an array of op_t structs */
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
-    /* Find the operator in the ops array */
-    while (ops[i].op != NULL && *ops[i].op != *s)
-        i++;
+	int i = 0;
 
-    /* Return the matching function */
-    return (ops[i].f);
+	/* Find the operator in the ops array */
+	while (ops[i].op != NULL && *ops[i].op != *s)
+		i++;
+
+	/* Return the matching function */
+	return (ops[i].f);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -6,8 +6,9 @@
  * @b: second integer
  * Return: sum of a and b
  */
-int op_add(int a, int b) {
-    return (a + b);
+int op_add(int a, int b)
+{
+	return (a + b);
 }
 
 /**
@@ -16,8 +17,9 @@ int op_add(int a, int b) {
  * @b: second integer
  * Return: difference of a and b
  */
-int op_sub(int a, int b) {
-    return (a - b);
+int op_sub(int a, int b)
+{
+	return (a - b);
 }
 
 /**
@@ -26,8 +28,9 @@ int op_sub(int a, int b) {
  * @b: second integer
  * Return: product of a and b
  */
-int op_mul(int a, int b) {
-    return (a * b);
+int op_mul(int a, int b)
+{
+	return (a * b);
 }
 
 /**
@@ -36,8 +39,9 @@ int op_mul(int a, int b) {
  * @b: second integer
  * Return: division of a by b
  */
-int op_div(int a, int b) {
-    return (a / b);
+int op_div(int a, int b)
+{
+	return (a / b);
 }
 
 /**
@@ -46,6 +50,7 @@ int op_div(int a, int b) {
  * @b: second integer
  * Return: remainder of the division of a by b
  */
-int op_mod(int a, int b) {
-    return (a % b);
+int op_mod(int a, int b)
+{
+	return (a % b);
 }
