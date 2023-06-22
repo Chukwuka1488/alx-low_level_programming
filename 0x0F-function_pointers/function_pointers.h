ce3debd72787333316d8a55ec0690492c295680a
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-06-22 17:01:31 +0200

    Write a program that performs simple operations.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -9,7 +9,7 @@
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-// Function prototype for the function to get operator
+/* Function prototype for the function to get operator */
 int (*get_op_func(char *s))(int, int);
 
 #endif
