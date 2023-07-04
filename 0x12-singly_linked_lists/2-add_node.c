c73d16d57296403922edf072100a65b24eb80c42
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-07-04 14:14:08 -0700

    a function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,21 +11,21 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    unsigned int len = 0;
+	list_t *new_node;
+	unsigned int len = 0;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    while (str[len])
-        len++;
+	while (str[len])
+		len++;
 
-    new_node->str = strdup(str);
-    new_node->len = len;
-    new_node->next = *head;
+	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = *head;
 
-    *head = new_node;
+	*head = new_node;
 
-    return (new_node);
+	return (new_node);
 }
