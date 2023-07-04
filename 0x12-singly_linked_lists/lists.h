65894483310ddcb310e8a5f6640fe23b36c47692
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-07-04 22:27:01 +0200

    a function that prints all the elements of a list_t list.

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,17 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/* Structure for a linked list node */
+typedef struct list_s
+{
+    char *str;
+    size_t len;
+    struct list_s *next;
+} list_t;
+
+/* Function prototypes */
+size_t print_list(const list_t *h);
+
+#endif /* LISTS_H */
