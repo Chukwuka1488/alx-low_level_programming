b9ab0868a8c2670b69a39d73cdfe18aef9acefc2
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-07-04 23:29:03 +0200

    64-bit program in assembly that prints Hello, Holberton, followed by a new line.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+
+/*
+ * File: 100-first.c
+ * Author: Your Name
+ * Description: Implementation file for pre-main function.
+ *              This function prints a message before the 
+ *              main function is executed.
+ */
+
+void pre_main(void)
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+    hello db "Hello, Holberton", 0
+
+section .text
+    extern printf
+
+    global main
+    main:
+        sub rsp, 8
+        mov rdi, hello
+        xor eax, eax
+        call printf
+
+        add rsp, 8
+        mov eax, 0x60
+        xor edi, edi
+        syscall
