225edc1f4dd4a97ed28eca9f1d31f7029449ef9d
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-07-11 21:41:04 +0200

    file copy updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -32,7 +32,7 @@ int open_file(const char *filename, int flags, mode_t mode)
 	fd = open(filename, flags, mode);
 	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't %s %s\n",
+		dprintf(STDERR_FILENO, "Error: Can't %s file %s\n",
 				(flags & O_CREAT) ? "write to" : "read from", filename);
 		exit((flags & O_CREAT) ? 99 : 98);
 	}
@@ -45,7 +45,7 @@ int open_file(const char *filename, int flags, mode_t mode)
  * @fd_from: The file descriptor of the source file.
  * @fd_to: The file descriptor of the destination file.
  */
-void copy_file(int fd_from, int fd_to)
+void copy_file(int fd_from, int fd_to, const char *file_from, const char *file_to)
 {
 	ssize_t n_read, n_written;
 	char buffer[BUFFER_SIZE];
@@ -54,10 +54,16 @@ void copy_file(int fd_from, int fd_to)
 	{
 		n_written = write(fd_to, buffer, n_read);
 		if (n_written == -1 || n_written != n_read)
-			error_exit("Error: Can't write to", 99);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
 	}
 	if (n_read == -1)
-		error_exit("Error: Can't read from", 98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
 }
 
 /**
@@ -84,7 +90,7 @@ int main(int argc, char *argv[])
 	fd_to = open_file(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	/* Copy the content from the source file to the destination file */
-	copy_file(fd_from, fd_to);
+	copy_file(fd_from, fd_to, file_from, file_to);
 
 	/* Close the source and destination files */
 	if (close(fd_from) == -1)
